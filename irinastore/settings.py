cd4040281d46d0d0c29d5dbdc77f2bb63a180925
peredniokas <peredniokas23@gmail.com>
2022-11-30 14:08:49 +0200

    modelis perrasystas is lenteliu i models.py modeli

--- irinastore/settings.py
+++ irinastore/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'bandymai',
     'store',
     'tags',
+    'likes',
     'debug_toolbar',
 ]
 
--- store/models.py
+++ store/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 
-
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount= models.FloatField()
 
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
 class Product(models.Model):
@@ -15,6 +18,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 
 class Customer(models.Model):
