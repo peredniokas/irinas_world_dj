30d9555abc80ded90fa2d18b0bfe8da84be40928
peredniokas <peredniokas23@gmail.com>
2022-12-01 13:48:02 +0200

    admin tvarkymas

--- irinastore/settings.py
+++ irinastore/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'tags',
     'likes',
     'debug_toolbar',
+    'store_custom',
 ]
 
 MIDDLEWARE = [
--- tags/models.py
+++ tags/models.py
@@ -2,12 +2,6 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
-from django.db import models
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import GenericForeignKey
-
 
 class TaggedItemManager(models.Manager):
     def get_tags_for(self, obj_type, obj_id):
@@ -24,16 +18,12 @@ class TaggedItemManager(models.Manager):
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.label
+
 
 class TaggedItem(models.Model):
     objects = TaggedItemManager()
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey()
-
-
-class TaggedItem(models.Model):
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
