422d35dfea7f448bfd46e90ac5ab1bed547de13f
peredniokas <peredniokas23@gmail.com>
2022-12-02 09:41:14 +0200

    product list restfrm

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-
+@api_view()
 def product_list(request):
-    return HttpResponse('all_good')
+    return Response('All good in a hood !:)')
