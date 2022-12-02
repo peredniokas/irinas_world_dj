ee7bd5abfad6250a2bdfe5bf45f1db0ef0ec72bf
peredniokas <peredniokas23@gmail.com>
2022-12-02 20:14:47 +0200

    serializers pagination filter

--- irinastore/settings.py
+++ irinastore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'bandymai',
     'store',
@@ -135,3 +136,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
