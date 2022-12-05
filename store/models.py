98c5825b91d218563b2d3e49a38e1c683274f7c6
peredniokas <peredniokas23@gmail.com>
2022-12-05 08:46:12 +0200

    user call

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.db import models
@@ -64,11 +65,15 @@ class Customer(models.Model):
         return f'{self.user.first_name} {self.user.last_name}'
 
 
+    @admin.display(ordering='user__first_name')
     def first_name(self):
         return self.user.first_name
 
+
+    @admin.display(ordering='user__last_name')
     def last_name(self):
         return self.user.last_name
+
 
     class Meta:
         ordering = ['user__first_name', 'user__last_name']
@@ -88,6 +93,11 @@ class Order(models.Model):
     payment_status = models.CharField(
         max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+    class Meta:
+        permissions = [
+            ('cancel_order', 'Can cancel order')
+        ]
 
 
 class OrderItem(models.Model):
