df72355241c6a08a1daf042ab876b5ea1a8a1a6f
peredniokas <peredniokas23@gmail.com>
2022-12-06 13:25:48 +0200

    media ,images upload

--- store/views.py
+++ store/views.py
@@ -11,8 +11,8 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework import status
 from .filters import ProductFilter
-from .models import Cart, CartItem, Collection, Customer, Product, Review, Order
-from .serializers import AddCartItemSerializer, OrderSerializer, CartItemSerializer, UpdateOrderSerializer, CreateOrderSerializer, CartSerializer, CollectionSerializer, CustomerSerializer, ProductSerializer, ReviewSerializer, UpdateCartItemSerializer
+from .models import Cart, CartItem, Collection, Customer, Product, Review, Order, ProductImage
+from .serializers import AddCartItemSerializer, OrderSerializer, CartItemSerializer,ProductImageSerializer, UpdateOrderSerializer, CreateOrderSerializer, CartSerializer, CollectionSerializer, CustomerSerializer, ProductSerializer, ReviewSerializer, UpdateCartItemSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -145,3 +145,13 @@ class OrderViewSet(ModelViewSet):
         customer_id = Customer.objects.only(
             'id').get(user_id=user.id)
         return Order.objects.filter(customer_id=customer_id)
+
+
+class ProductImageViewSet(ModelViewSet):
+    serializer_class = ProductImageSerializer
+
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
+
+    def get_queryset(self):
+        return ProductImage.objects.filter(product_id=self.kwargs['product_pk'])
