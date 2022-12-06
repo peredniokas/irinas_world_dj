3b556128aff6d673cc07d7e5404e0637b1934102
peredniokas <peredniokas23@gmail.com>
2022-12-06 13:55:23 +0200

    validating uploaded images (files)

--- store/models.py
+++ store/models.py
@@ -4,6 +4,8 @@ from django.core.validators import MinValueValidator
 from django.db import models
 from uuid import uuid4
 
+from store.validators import validate_file_size
+
 
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
@@ -47,7 +49,8 @@ class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(
-        upload_to='store/images')
+        upload_to = 'store/images',
+        validators=[validate_file_size])
 
 
 class Customer(models.Model):
@@ -60,27 +63,23 @@ class Customer(models.Model):
         (MEMBERSHIP_SILVER, 'Silver'),
         (MEMBERSHIP_GOLD, 'Gold'),
     ]
-    
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True, blank=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
-
 
     @admin.display(ordering='user__first_name')
     def first_name(self):
         return self.user.first_name
 
-
     @admin.display(ordering='user__last_name')
     def last_name(self):
         return self.user.last_name
-
 
     class Meta:
         ordering = ['user__first_name', 'user__last_name']
@@ -107,12 +106,12 @@ class Order(models.Model):
     class Meta:
         permissions = [
             ('cancel_order', 'Can cancel order')
-            
         ]
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='items')
+    order = models.ForeignKey(
+        Order, on_delete=models.PROTECT, related_name='items')
     product = models.ForeignKey(
         Product, on_delete=models.PROTECT, related_name='orderitems')
     quantity = models.PositiveSmallIntegerField()
